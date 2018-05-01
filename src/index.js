7f07c761bbc5dc9afc93af49f9cb8131f713c340
CloudRex <cloudrex@outlook.com>
2018-04-30 23:32:10 -0400

    Added the Feature class to the index.js file

--- src/index.js
+++ src/index.js
@@ -1,48 +1,49 @@
 module.exports = {
-    // Commands
-    CommandArgumentParser: require("./commands/command-argument-parser").default,
-    CommandCategoryType: require("./commands/command-category-type").default,
-    CommandExecutionContext: require("./commands/command-execution-context").default,
-    CommandLoader: require("./commands/command-loader").default,
-    CommandManager: require("./commands/command-manager").default,
-    CommandParser: require("./commands/command-parser").default,
-    Command: require("./commands/command").default,
+	// Commands
+	CommandArgumentParser: require("./commands/command-argument-parser").default,
+	CommandCategoryType: require("./commands/command-category-type").default,
+	CommandExecutionContext: require("./commands/command-execution-context").default,
+	CommandLoader: require("./commands/command-loader").default,
+	CommandManager: require("./commands/command-manager").default,
+	CommandParser: require("./commands/command-parser").default,
+	Command: require("./commands/command").default,
 
-    // Console
-    ConsoleInterface: require("./console/console-interface").default,
-    ConsoleCommand: require("./console/console-command").default,
+	// Console
+	ConsoleInterface: require("./console/console-interface").default,
+	ConsoleCommand: require("./console/console-command").default,
 
-    // Core
-    AccessLevelType: require("./core/access-level-type").default,
-    Bot: require("./core/bot").default,
-    EditableMessage: require("./core/editable-message").default,
-    EmbedBuilder: require("./core/embed-builder").default,
-    Log: require("./core/log").default,
-    MessageBuilder: require("./core/message-builder").default,
-    RGB: require("./core/rgb").default,
-    RGBA: require("./core/rgba").default,
-    Settings: require("./core/settings").default,
-    TimeParser: require("./core/time-parser").default,
-    TimeSuffixType: require("./core/time-suffix-type").default,
-    UserConfig: require("./core/user-config").default,
-    Utils: require("./core/utils").default,
+	// Core
+	AccessLevelType: require("./core/access-level-type").default,
+	Bot: require("./core/bot").default,
+	EditableMessage: require("./core/editable-message").default,
+	EmbedBuilder: require("./core/embed-builder").default,
+	Log: require("./core/log").default,
+	MessageBuilder: require("./core/message-builder").default,
+	RGB: require("./core/rgb").default,
+	RGBA: require("./core/rgba").default,
+	Settings: require("./core/settings").default,
+	TimeParser: require("./core/time-parser").default,
+	TimeSuffixType: require("./core/time-suffix-type").default,
+	UserConfig: require("./core/user-config").default,
+	Utils: require("./core/utils").default,
 
-    // Collections
+	// Collections
 	Collecton: require("./collections/collection").default,
 	EmojiCollection: require("./collections/emoji-collection").default,
 
-    // Emoji UI
-    EmojiButton: require("./emoji-ui/emoji-button").default,
-    EmojiMenuManager: require("./emoji-ui/emoji-menu-manager").default,
-    EmojiMenu: require("./emoji-ui/emoji-menu").default,
+	// Emoji UI
+	EmojiButton: require("./emoji-ui/emoji-button").default,
+	EmojiMenuManager: require("./emoji-ui/emoji-menu-manager").default,
+	EmojiMenu: require("./emoji-ui/emoji-menu").default,
 
-    // Events
-    CommandWillExecuteEvent: require("./events/command-will-execute-event").default,
-    CommandExecutedEvent: require("./events/command-executed-event").default,
+	// Events
+	CommandWillExecuteEvent: require("./events/command-will-execute-event").default,
+	CommandExecutedEvent: require("./events/command-executed-event").default,
 
-    // Features
-    FeatureManager: require("./features/feature-manager").default,
+	// Features
+	FeatureManager: require("./features/feature-manager").default,
+	Feature: require("./features/feature").default,
 
-    // Schema
-    SchemaValidator: require("./schema/schema-validator").default,
+	// Schema
+	SchemaValidator: require("./schema/schema-validator").default,
 };

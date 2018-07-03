04e6636af84e046b15cf1e8c3f0a1074d45061fa
CloudRex <cloudrex@outlook.com>
2018-07-03 08:45:34 -0400

    Added the usage primitive command

---
+++ src/commands/primitives/usage.ts
@@ -0,0 +1,40 @@
+import {default as Command, CommandOptions} from "../command";
+import CommandExecutionContext from "../command-execution-context";
+
+const command: CommandOptions = {
+    meta: {
+        name: "usage",
+        desc: "View how to use a certain command",
+
+        args: {
+            command: "!:string"
+        }
+    },
+
+    executed: (context: CommandExecutionContext): void => {
+        const targetCommand: Command | null = context.bot.commands.getByName(context.arguments[0]);
+
+        if (!targetCommand) {
+            context.fail("That command doesn't exist.");
+
+            return;
+        }
+        else if (!targetCommand.args) {
+            context.fail("That command doesn't accept any arguments.");
+
+            return;
+        }
+
+        const argKeys: Array<string> = Object.keys(targetCommand.args);
+
+        let args: Array<string> = [];
+
+        for (let i: number = 0; i < argKeys.length; i++) {
+            args.push(argKeys[i].startsWith("!") ? argKeys[i] : `[${argKeys[i]}]`);
+        }
+
+        context.ok(args.join(" "));
+    }
+};
+
+export default command;

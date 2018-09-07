eab5ddf9f559bc538c789dd8c5858902eac59367
CloudRex <cloudrex@outlook.com>
2018-09-07 12:57:43 -0400

    Improved postinstall test script

--- tasks/docker-test.js
+++ tasks/docker-test.js
@@ -2,4 +2,5 @@ console.log("=== docker test is running ===");
 
 const fs = require("fs");
 
-console.log(fs.readdirSync("/app/node_modules/discord-anvil"));
+console.log("under WORKING, ", fs.readdirSync("./"));
+console.log("under discord anvil, ", fs.readdirSync("/app/node_modules/discord-anvil"));

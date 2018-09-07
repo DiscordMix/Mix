92baea060e4c755fa3695c19c5100152c4702b7a
CloudRex <cloudrex@outlook.com>
2018-09-07 13:35:37 -0400

    Removed docker test script

--- tasks/docker-test.js
+++
@@ -1,6 +0,0 @@
-console.log("=== docker test is running ===");
-
-const fs = require("fs");
-
-console.log("under WORKING, ", fs.readdirSync("./"));
-console.log("under discord anvil, ", fs.readdirSync("/app/node_modules/discord-anvil"));

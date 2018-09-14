75e65953d658f3637bee1956df77fc1b68298e8f
CloudRex <cloudrex@outlook.com>
2018-09-13 23:34:41 -0400

    Added use mention pattern

--- src/core/patterns.ts
+++ src/core/patterns.ts
@@ -4,4 +4,6 @@ export default abstract class Patterns {
     public static url: RegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
 
     public static token: RegExp = /^[a-zA-Z0-9]{24}\.[a-zA-Z0-9]{3}\-[a-z]{2}\.[_a-zA-Z0-9-]{27}$/gm;
+
+    public static mention: RegExp = /<@[0-9]+>/gm;
 }

af2dc1b9fe9979e18f458021253132466ce2b40c
CloudRex <cloudrex@outlook.com>
2018-09-26 12:26:53 -0400

    Stringified debug object for inspection

--- src/decorators/decorators.ts
+++ src/decorators/decorators.ts
@@ -65,7 +65,7 @@ export function on(eventName: DiscordEvent | string) {
     return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
         console.log("meta is ", target.meta);
 
-        console.log("new constructor is ", new target());
+        console.log("jsonied is ", JSON.stringify(target));
 
         BotEvents.push({
             name: eventName,

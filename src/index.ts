a73244a1d21b4fa3bd065be8baba1a125600d1e4
CloudRex <private.phantom@outlook.com>
2018-10-13 00:44:30 -0400

    Added missing export

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ import Command, {
     PrimitiveArgType,
     CustomArgType,
     ArgumentResolver,
-    RestrictGroup
+    RestrictGroup,
+    InternalArgType
 } from "./commands/command";
 
 import ConsoleInterface from "./console/console-interface";
@@ -66,6 +67,7 @@ export {
     PrimitiveArgType,
     ArgumentResolver,
     RestrictGroup,
+    InternalArgType,
 
     // Console
     ConsoleInterface,

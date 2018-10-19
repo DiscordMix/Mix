e61eaef804ff4e06b866cff304c1657337fa10fd
CloudRex <private.phantom@outlook.com>
2018-10-18 21:17:00 -0400

    Small improvements on the docs

--- src/builders/embed-builder.ts
+++ src/builders/embed-builder.ts
@@ -152,7 +152,7 @@ export default class EmbedBuilder {
     }
 
     /**
-     * @param {Object} sections
+     * @param {*} sections
      * @param {string} color
      * @return {EmbedBuilder}
      */

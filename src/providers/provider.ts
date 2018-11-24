9eff4dbc1929eb1839fcddf3fad04375945bf52f
CloudRex <cloudrex@outlook.com>
2018-11-24 04:35:08 -0500

    Add has method to the provider interface

--- src/providers/provider.ts
+++ src/providers/provider.ts
@@ -17,12 +17,19 @@ export type IProvider<ItemType> = {
      * @return {boolean}
      */
     set(key: string, value: ItemType): boolean;
+
+    /**
+     * @param {string} key
+     * @return {PromiseOr<boolean>}
+     */
+    has(key: string): PromiseOr<boolean>;
 }
 
 export interface IDuplexProvider<ItemType> extends IProvider<ItemType> {
     /**
      * @param {string} key
      * @param {ItemType} value
+     * @return {boolean}
      */
     update(key: string, value: ItemType): boolean;
 
@@ -36,38 +43,38 @@ export interface IDuplexProvider<ItemType> extends IProvider<ItemType> {
 export interface IQueriableProvider<ItemType> extends IProvider<ItemType> {
     /**
      * @param {Partial<ItemType>} query
-     * @return {ItemType[] | null}
+     * @return {PromiseOr<ItemType[] | null>}
      */
     find(query: Partial<ItemType>): PromiseOr<ItemType[] | null>;
 
     /**
      * @param {Partial<ItemType>} query
-     * @return {ItemType | null}
+     * @return {PromiseOr<ItemType | null>}
      */
     findOne(query: Partial<ItemType>): PromiseOr<ItemType | null>;
 
     /**
      * @param {Partial<ItemType>} query
-     * @param {number} value
+     * @param {PromiseOr<number>} value
      */
     update(query: Partial<ItemType>, value: ItemType): PromiseOr<number>;
 
     /**
      * @param {Partial<ItemType>} query
      * @param {ItemType} value
-     * @return {boolean}
+     * @return {PromiseOr<boolean>}
      */
     updateOne(query: Partial<ItemType>, value: ItemType): PromiseOr<boolean>;
 
     /**
      * @param {Partial<ItemType>} query
-     * @return {number}
+     * @return {PromiseOr<number>}
      */
     delete(query: Partial<ItemType>): PromiseOr<number>;
 
     /**
      * @param {Partial<ItemType>} query
-     * @return {boolean}
+     * @return {PromiseOr<boolean>}
      */
     deleteOne(query: Partial<ItemType>): PromiseOr<boolean>;
 }

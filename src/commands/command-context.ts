633a750f7e56358bd12a2acf8826dfde3a45e26f
CloudRex <cloudrex@outlook.com>
2018-11-26 18:28:06 -0500

    Add request helper method

--- src/commands/command-context.ts
+++ src/commands/command-context.ts
@@ -87,4 +87,32 @@ export default class CommandContext<DataType = any, ChannelType = TextChannel | 
     public async privateReply(message: string): Promise<Message | Message[]> {
         return await this.msg.author.send(Utils.escapeText(message, this.bot.client.token));
     }
+
+    /**
+     * Request an input from the user
+     * @param {string} message The message to send
+     * @param {Snowflake} from The user to expect response from
+     * @param {number} [timeout=7500] The time to wait until automatic cancellation
+     * @return {Promise<string | null>}
+     */
+    public async request(message: string, from: Snowflake, timeout: number = 7500): Promise<string | null> {
+        return new Promise<string | null>(async (resolve) => {
+            const responseTimeout: NodeJS.Timeout = this.bot.setTimeout(() => {
+                this.bot.client.removeListener("message", listener);
+                resolve(null);
+            }, timeout);
+    
+            const listener: any = (msg: Message) => {
+                if (msg.author.id !== from || !msg.content || typeof msg.content !== "string" || msg.content.startsWith("|")) {
+                    return;
+                }
+    
+                this.bot.clearTimeout(responseTimeout);
+                resolve(msg.content);
+            };
+    
+            this.bot.client.on("message", listener);
+            await this.msg.channel.send(message);
+        });
+    }
 }

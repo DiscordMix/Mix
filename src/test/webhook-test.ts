deeb6acd03854a28a7a6c2a8304d3598651353b3
CloudRex <cloudrex@outlook.com>
2018-12-26 13:44:48 -0500

    Add test operations

--- src/test/webhook-test.ts
+++ src/test/webhook-test.ts
@@ -2,6 +2,7 @@ import {Coordinator, GithubEvent, ICoordinatorRunResult, CoordinatorState} from 
 import ScriptOperations from "../automation/predefied-ops/scripts";
 import GitOperations from "../automation/predefied-ops/git";
 import FileSystemOperations from "../automation/predefied-ops/file-system";
+import TestOperations from "../automation/predefied-ops/test";
 
 const coordinator: Coordinator = new Coordinator();
 const secret: string = "keyboard_cat";
@@ -18,6 +19,7 @@ const githubPort: number = coordinator.githubWebhook(secret, async (event: Githu
     const buildDir: string = "./dist";
 
     const result: ICoordinatorRunResult = await coordinator
+        .then(TestOperations.testFalseSync)
         .then(() => GitOperations.branch(masterBranch))
         .then(() => GitOperations.deleteBranch(deployBranch))
         .then(() => GitOperations.createBranch(deployBranch))

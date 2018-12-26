fe6b57e9956a76d03dc050253b61ccf4dfc5bd62
CloudRex <cloudrex@outlook.com>
2018-12-26 01:54:00 -0500

    Auto update upon trigger

--- src/test/webhook-test.ts
+++ src/test/webhook-test.ts
@@ -1,10 +1,22 @@
-import {Coordinator, GithubEvent} from "../automation/coordinator";
+import {Coordinator, GithubEvent, ICoordinatorRunResult, CoordinatorState} from "../automation/coordinator";
+import ScriptOperations from "../automation/predefied-ops/scripts";
+import GitOperations from "../automation/predefied-ops/git";
+import FileSystemOperations from "../automation/predefied-ops/file-system";
 
 const coordinator: Coordinator = new Coordinator();
 const secret: string = "keyboard_cat";
 
-const githubPort: number = coordinator.githubWebhook(secret, (event: GithubEvent, body: any) => {
-    console.log(`Github | Received webhook trigger | Event is ${event}`);
+const githubPort: number = coordinator.githubWebhook(secret, async (event: GithubEvent, body: any) => {
+    console.log(`Github | Processing webhook trigger | Event is ${event}`);
+
+    const result: ICoordinatorRunResult = await coordinator
+        .onlyIf(ScriptOperations.npmTest)
+        .then(GitOperations.pull)
+        .then(() => FileSystemOperations.forceRemove("./dist"))
+        .then(ScriptOperations.npmInstall)
+        .run();
+
+    console.log(`Github | Process completed | Result is ${result.state === CoordinatorState.OK ? "OK" : "Failed"}`, result);
 });
 
 const normalPort: number = coordinator.webhook((body: any) => {

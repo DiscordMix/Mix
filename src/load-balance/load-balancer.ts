5cfde38578a669fb067467168478cb6e872d6605
CloudRex <cloudrex@outlook.com>
2018-12-30 23:45:45 -0500

    Add partial load balancer class

--- src/load-balance/load-balancer.ts
+++ src/load-balance/load-balancer.ts
@@ -1,6 +1,10 @@
 import {EventEmitter} from "events";
+import {PriorityQueue} from "@atlas/xlib";
+import {Action} from "../core/bot-extra";
 
 export interface ILoadBalancer extends EventEmitter {
+    next(action: Action): this;
+
     readonly threshold: number;
     readonly requestsPerSecond: number;
     readonly requestsPerMinute: number;
@@ -10,10 +14,28 @@ export interface ILoadBalancer extends EventEmitter {
 export default class LoadBalancer extends EventEmitter implements ILoadBalancer {
     public readonly threshold: number;
 
+    private readonly queue: PriorityQueue<Action>;
+
     public constructor(threshold: number = 10) {
         super();
 
         this.threshold = threshold;
+        this.queue = new PriorityQueue();
+    }
+
+    protected async process(): Promise<this> {
+        // TODO: Finish implementing
+        /* for (const action of this.queue) {
+            await action();
+        } */
+
+        return this;
+    }
+
+    public next(action: Action, highPriority?: boolean): this {
+        this.queue.enqueue(action, highPriority);
+
+        return this;
     }
 
     // TODO
@@ -21,10 +43,12 @@ export default class LoadBalancer extends EventEmitter implements ILoadBalancer 
         return 0;
     }
 
+    // TODO
     public get requestsPerMinute(): number {
         return 0;
     }
 
+    // TODO
     public get requestsPerHour(): number {
         return 0;
     }

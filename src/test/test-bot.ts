54c6f1aed495362eb5d40a24ec33676f8a25341f
CloudRex <cloudrex@outlook.com>
2019-01-05 14:53:17 -0500

    Add time convert class and tests

--- src/test/test-bot.ts
+++ src/test/test-bot.ts
@@ -23,6 +23,7 @@ import Task from "../tasks/task";
 import {EBotEvents} from "../core/bot-extra";
 import {IStateCapsule} from "../state/time-machine";
 import Delta from "../state/delta";
+import TimeConvert from "../time/time-convert";
 
 // Test globals
 const globalAny: any = global;
@@ -700,6 +701,42 @@ describe("Log Serializer", () => {
             expect(result.source.main).to.be.an("string").and.to.equal("Some");
             expect(result.source.extra).to.be.a("undefined");
             expect(result.time).to.be.a("string").and.to.equal("Today");
+        });
+    });
+});
+
+describe("Time Convert", () => {
+    describe("format()", () => {
+        it("should format specified time", () => {
+            expect(TimeConvert.format(1_000, "%i")).to.be.a("string").and.to.equal("1000");
+            expect(TimeConvert.format(1_000, "%s")).to.be.a("string").and.to.equal("1");
+            expect(TimeConvert.format(1_000 * 60, "%m")).to.be.a("string").and.to.equal("1");
+            expect(TimeConvert.format(1_000 * 60 * 60, "%h")).to.be.a("string").and.to.equal("1");
+            expect(TimeConvert.format(1_000 * 60 * 60 * 60, "%h")).to.be.a("string").and.to.equal("60");
+
+            expect(TimeConvert.format(1_000, "ms: %i, seconds: %s")).to.be.a("string").and.to.equal("ms: 1000, seconds: 1");
+            expect(TimeConvert.format(1_000, "ms: %i (%i), seconds: %s (%s)")).to.be.a("string").and.to.equal("ms: 1000 (1000), seconds: 1 (1)");
+        });
+
+        it("should throw on invalid parameters", () => {
+            assert.throws(() => TimeConvert.format(undefined as any, "test"));
+            assert.throws(() => TimeConvert.format("test" as any, undefined as any));
+            assert.throws(() => TimeConvert.format(1 as any, undefined as any));
+            assert.throws(() => TimeConvert.format(null as any, undefined as any));
+            assert.throws(() => TimeConvert.format({} as any, undefined as any));
+            assert.throws(() => TimeConvert.format([] as any, undefined as any));
+            assert.throws(() => TimeConvert.format("" as any, undefined as any));
+            assert.throws(() => TimeConvert.format(0 as any, undefined as any));
+            assert.throws(() => TimeConvert.format(true as any, undefined as any));
+            assert.throws(() => TimeConvert.format(false as any, undefined as any));
+
+            assert.throws(() => TimeConvert.format(1, null as any));
+            assert.throws(() => TimeConvert.format(1, 1 as any));
+            assert.throws(() => TimeConvert.format(1, 0 as any));
+            assert.throws(() => TimeConvert.format(1, true as any));
+            assert.throws(() => TimeConvert.format(1, false as any));
+            assert.throws(() => TimeConvert.format(1, {} as any));
+            assert.throws(() => TimeConvert.format(1, [] as any));
         });
     });
 });

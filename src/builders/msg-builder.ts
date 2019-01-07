c4c0dbabf937225ccf786dd94b952ca23121379a
CloudRex <cloudrex@outlook.com>
2019-01-07 16:37:39 -0500

    Improve message builder

--- src/builders/msg-builder.ts
+++ src/builders/msg-builder.ts
@@ -1,6 +1,7 @@
 import {IBuilder} from "./builder";
+import {Log} from "..";
 
-export interface IMsgBuilder extends IBuilder<string> {
+export interface IMsgBuilder<T> extends IBuilder<string> {
     append(text: string): this;
     add(text: string): this;
     block(language?: string): this;
@@ -11,10 +12,15 @@ export interface IMsgBuilder extends IBuilder<string> {
     strike(text: string): this;
     line(amount: number): this;
     emoji(emoji: string): this;
+    format(callback: FormatterCallback<T>): this;
+    feed(item: T): this;
 }
 
-export default class MsgBuilder implements IMsgBuilder {
+export type FormatterCallback<T> = (item: T) => string;
+
+export default class MsgBuilder<T = any> implements IMsgBuilder<T> {
     protected message: string;
+    protected formatter?: FormatterCallback<T>;
 
     /**
      * @param {string} [value=""]
@@ -123,6 +129,22 @@ export default class MsgBuilder implements IMsgBuilder {
      */
     public emoji(emoji: string): this {
         return this.append(`:${emoji}:`);
+    }
+
+    public format(callback: FormatterCallback<T>): this {
+        this.formatter = callback;
+
+        return this;
+    }
+
+    public feed(item: T): this {
+        if (this.formatter === undefined || typeof this.formatter !== "function") {
+            throw Log.error("Expecting formatter to be set and to be a function");
+        }
+
+        this.append(this.formatter(item));
+
+        return this;
     }
 
     /**

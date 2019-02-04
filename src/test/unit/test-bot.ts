bdc9f28ff00155feecc08c398f20f887be505aec
CloudRex <cloudrex@outlook.com>
2019-02-03 23:42:42 -0500

    Remove fragile bot tests

--- src/test/unit/test-bot.ts
+++ src/test/unit/test-bot.ts
@@ -1,18 +1,13 @@
 require("dotenv").config();
 
-import {Guild, Message, Snowflake, TextChannel} from "discord.js";
 import path from "path";
-import Context from "../../commands/command-context";
 import Bot from "../../core/bot";
 import {BotEvent} from "../../core/bot-extra";
-import BotMessages from "../../core/messages";
-import ResponseHelper from "../../core/response-helper";
 import Settings from "../../core/settings";
 import Rgb from "../../misc/rgb";
 import Rgba from "../../misc/rgba";
 import {ITestState, TestStoreActionType} from "../../state/store";
 import List from "../../collections/list";
-import Log from "../../core/log";
 
 export const TestSubjects = {
     ids: [
@@ -53,94 +48,21 @@ export const TestSubjects = {
     }
 };
 
-const token: string = process.env.TEST_BOT_TOKEN as string;
-const testGuildId: Snowflake = process.env.TEST_GUILD_ID as Snowflake;
-const testGuildChannelId: Snowflake = process.env.TEST_CHANNEL_ID as Snowflake;
-
 /*
     TODO: The Optimizer's interval isn't getting
     cleared at bot.dispose() (on shutdown) therefore leaving tests hanging.
     Hotfixed by disabling optimizer in tests.
 */
 
-if (!token) {
-    throw Log.error(BotMessages.TEST_EXPECT_TOKEN);
-}
-else if (!testGuildId) {
-    throw Log.error(BotMessages.TEST_EXPECT_GUILD);
-}
-else if (!testGuildChannelId) {
-    throw Log.error(BotMessages.TEST_EXPECT_CHANNEL);
-}
-
 export default class TestBot extends Bot<ITestState, TestStoreActionType> {
-    public static testGuild: Guild;
-    public static testChannel: TextChannel;
-
-    public async deleteLastMessage(): Promise<void> {
-        if (!this.client.user) {
-            return;
-        }
-
-        const lastMessage: Message = this.getLastMessage();
-
-        if (!lastMessage) {
-            return;
-        }
-        else if (lastMessage.deletable) {
-            await lastMessage.delete();
-        }
-    }
-
-    public getLastMessage(): Message {
-        return this.client.user.lastMessage;
-    }
-
-    public getContext(): Context {
-        return new Context({
-            bot: this,
-            label: "?",
-
-            // TODO: Should we check for a message?
-            msg: this.getLastMessage() as Message
-        });
-    }
-
-    public getResponseHelper(): ResponseHelper {
-        return new ResponseHelper(TestBot.testChannel, this, this.client.user);
-    }
-
-    // Tests
-    public async $sendEmbedMessageOk(): Promise<Message> {
-        await this.getResponseHelper().ok("hello world");
-
-        return this.getLastMessage();
-    }
-
-    public async $sendEmbedMessageFail(): Promise<Message> {
-        await this.getResponseHelper().fail("failed message");
-
-        return this.getLastMessage();
-    }
-
-    public async $sendEmbedMessageSend(): Promise<Message> {
-        await this.getResponseHelper().send("hello world");
-
-        return this.getLastMessage();
-    }
-
-    public async $longMessages(msg: string): Promise<Message> {
-        await this.getResponseHelper().ok(msg);
-
-        return this.getLastMessage();
-    }
+    // TODO.
 }
 
 export let testBot: TestBot = new TestBot({
     settings: new Settings({
         general: {
             prefix: ["!"],
-            token
+            token: null as any
         }
     })
 }, true);
@@ -152,26 +74,5 @@ export async function init(): Promise<void> {
         });
 
         await testBot.connect();
-
-        // Retrieve test guild and channel
-        if (!testBot.client.guilds.has(testGuildId)) {
-            throw Log.error(BotMessages.TEST_NO_GUILD);
-        }
-
-        const testGuild: Guild = testBot.client.guilds.get(testGuildId) as Guild;
-
-        if (!testGuild.channels.has(testGuildChannelId)) {
-            throw Log.error(BotMessages.TEST_CHANNEL_NO_EXIST);
-        }
-
-        const testChannel: TextChannel = testGuild.channels.get(testGuildChannelId) as TextChannel;
-
-        if (testChannel.type !== "text") {
-            throw Log.error(BotMessages.TEST_CHANNEL_NOT_TEXT);
-        }
-
-        // Set channels
-        TestBot.testGuild = testGuild;
-        TestBot.testChannel = testChannel;
     });
 }

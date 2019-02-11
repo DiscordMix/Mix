84a720d84780d1c75da806c5e73e5023570d5b6b
CloudRex <cloudrex@outlook.com>
2019-02-11 18:27:03 -0500

    Improve test names

--- src/test/unit/uncategorized/collection.unit.ts
+++ src/test/unit/uncategorized/collection.unit.ts
@@ -5,13 +5,13 @@ import Settings from "../../../core/settings";
 @unit("Collection")
 default class {
     @test("at(): should return the item located in the specified index")
-    public at() {
+    public at_returnAtSpecified() {
         Assert.equal(TestSubjects.collection.at(0), "hello");
         Assert.equal(TestSubjects.collection.at(1), "it's me");
     }
 
     @test("removeAt(): should remove the item located in the specified index")
-    public removeAt() {
+    public removeAt_removeAtSpecified() {
         const result1: boolean = TestSubjects.collection.removeAt(0);
         const result2: boolean = TestSubjects.collection.removeAt(5);
 
@@ -21,7 +21,7 @@ default class {
     }
 
     @test("add(): should add an item to the collection")
-    public add() {
+    public add_addItem() {
         TestSubjects.collection.add("test");
 
         Assert.equal(TestSubjects.collection.at(0), "it's me");
@@ -29,19 +29,19 @@ default class {
     }
 
     @test("addUnique(): should add an unique item")
-    public addUnique() {
+    public addUnique_addItem() {
         Assert.true(TestSubjects.collection.addUnique("doe"));
         Assert.false(TestSubjects.collection.addUnique("doe"));
     }
 
     @test("contains(): should determine whether the collection contains an item")
-    public contains() {
+    public contains_determineWhetherExists() {
         Assert.true(TestSubjects.collection.contains("doe"));
         Assert.false(TestSubjects.collection.contains("nope"));
     }
 
     @test("find(): should find an item by its property")
-    public find() {
+    public find_findByProp() {
         const result: any = TestSubjects.collection.find("name", "John Doe");
 
         Assert.that(result, Is.object);

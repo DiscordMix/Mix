242740b71121fec0432027ef5b7440b1dc23fea3
CloudRex <cloudrex@outlook.com>
2019-03-02 22:45:53 -0500

    Revert "Use core namespace in WebhookTest"
    This reverts commit 9896a759f76516e26dd1191703befe0f286bcb41.

--- src/test/WebhookTest.ts
+++ src/test/WebhookTest.ts
@@ -1,4 +1,6 @@
 import {FileOps, Coordinator, GitOps, ScriptOps, GithubEvent, ITaskResult, RunState} from "@atlas/automata";
+import Util from "../core/Util";
+import Log from "../core/Log";
 import colors from "colors";
 
 const coordinator: Coordinator = new Coordinator();
@@ -10,11 +12,11 @@ const buildDir: string = "./dist";
 const githubPort: number = coordinator.githubWebhook(secret, async (event: GithubEvent, body: any) => {
     const ref: string = body.ref as string;
 
-    Core.Log.verbose(`Github | Processing webhook trigger to ref '${ref}' | Event is '${event}'`);
+    Log.verbose(`Github | Processing webhook trigger to ref '${ref}' | Event is '${event}'`);
 
     // Verify that action is being performed to desired branch(es)
     if (event !== GithubEvent.Push || !ref || !ref.endsWith(`/${masterBranch}`)) {
-        Core.Log.verbose("Github | Ignoring event");
+        Log.verbose("Github | Ignoring event");
 
         return;
     }
@@ -32,7 +34,7 @@ const githubPort: number = coordinator.githubWebhook(secret, async (event: Githu
         .then(ScriptOps.npmTest)
 
         .fallback(async () => {
-            Core.Log.verbose("Github | Fallback sequence initiated");
+            Log.verbose("Github | Fallback sequence initiated");
 
             const fallbackResult: ITaskResult = await coordinator
                 .then(() => GitOps.branch(masterBranch))
@@ -40,22 +42,22 @@ const githubPort: number = coordinator.githubWebhook(secret, async (event: Githu
 
                 .run();
 
-            Core.Log.verbose(`Github | Fallback sequence completed | Result is '${fallbackResult.state === RunState.OK ? colors.green("OK") : colors.red("FAIL")}'`);
+            Log.verbose(`Github | Fallback sequence completed | Result is '${fallbackResult.state === RunState.OK ? colors.green("OK") : colors.red("FAIL")}'`);
         })
 
         .run((current: number, left: number, total: number, percentage: number) => {
-            Core.Log.verbose(`Github | Processing action ${current}/${total} : ${percentage}% (${left} left)`);
+            Log.verbose(`Github | Processing action ${current}/${total} : ${percentage}% (${left} left)`);
         });
 
-    const time: string = Core.Util.spreadTime(result.time);
-    const avgTime: string = Core.Util.spreadTime(result.averageTime);
+    const time: string = Util.spreadTime(result.time);
+    const avgTime: string = Util.spreadTime(result.averageTime);
 
-    Core.Log.verbose(`Github | Process completed in ${time}ms (${avgTime}ms avg.) | Result is '${result.state === RunState.OK ? "OK" : "Failed"}'`);
+    Log.verbose(`Github | Process completed in ${time}ms (${avgTime}ms avg.) | Result is '${result.state === RunState.OK ? "OK" : "Failed"}'`);
 });
 
 const normalPort: number = coordinator.webhook((body: any) => {
-    Core.Log.verbose("Normal | Received webhook trigger with body", body);
+    Log.verbose("Normal | Received webhook trigger with body", body);
 }, secret);
 
-Core.Log.verbose(`Github webhook ready | Port ${githubPort}`);
-Core.Log.verbose(`Normal webhook ready | Port ${normalPort}`);
+Log.verbose(`Github webhook ready | Port ${githubPort}`);
+Log.verbose(`Normal webhook ready | Port ${normalPort}`);
